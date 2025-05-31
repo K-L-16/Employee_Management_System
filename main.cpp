ea905deab0ed8c3ff7b02734266ba4ea09ddd15d
K-L-16 <kevinliu20210000@gmail.com>
2025-05-30 20:54:07 -0400

    update main

---
+++ main.cpp
@@ -0,0 +1,14 @@
+﻿#include"ManageMent_gui.h"
+#include"widgets/Window.h"
+#include"widgets/Table.h"
+#include"widgets/Timer.h"
+int main()
+{
+	Window w(960, 640, EX_NOCLOSE);
+	w.setWindowTitle("Employee Management System");
+
+	ManageMent m;
+	m.run();
+
+	return w.exec();
+}
